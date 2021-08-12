391160689326ba230efda96a383636c2f419b427
chrismcgale <mcga6260@mylaurier.ca>
2021-08-12 14:17:26 -0400

    Tests started

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,8 +1,10 @@
+import moment from 'moment';
+
 const filtersReducerDefaultState = {
     text: '', 
-    sortBy: 'data',
-    startDate: undefined, 
-    endDate: undefined
+    sortBy: 'date',
+    startDate: moment().startOf('month'), 
+    endDate: moment().endOf('month')
 };
 
 export default (state = filtersReducerDefaultState, action) => {
